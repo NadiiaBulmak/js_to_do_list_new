3e0763d1976ba9ab6f86d48d0120ff19eaee591d
Nadiia Bulmak <nadgaval@gmail.com>
2025-01-29 19:06:56 +0200

    njnk

--- dist/index.665c58ab.js
+++
@@ -1,5 +0,0 @@
-const e=document.getElementById("input-box"),t=document.getElementById("list-container"),n=document.querySelector(".todo-app__button");function o(e){let t=e.target;t.classList.contains("todo-app__item")&&(t.classList.toggle("todo-app__item--checked"),i()),t.classList.contains("todo-app__item-remove")&&(t.parentElement.remove(),i())}function a(){let n=e.value.trim();if(!n){alert("You have to write something!");return}let o=document.createElement("li");o.className="todo-app__item",o.innerHTML=`
-        ${n}
-        <span class="todo-app__item-remove">&times;</span>
-    `,t.appendChild(o),e.value="",e.focus(),i()}function i(){localStorage.setItem("todoItems",t.innerHTML)}document.addEventListener("DOMContentLoaded",()=>{(function(){let e=localStorage.getItem("todoItems");e&&(t.innerHTML=e)})(),n.addEventListener("click",a),e.addEventListener("keypress",e=>{"Enter"===e.key&&a()}),t.addEventListener("click",o)});
-//# sourceMappingURL=index.665c58ab.js.map
